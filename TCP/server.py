00fd56d276398ad3be27631a81e7cbe6535ce6c9
ajk@mjolner.dk <ajk@mjolner.dk>
2019-09-10 16:37:55 +0200

    initial client/server

--- TCP/server.py
+++ TCP/server.py
@@ -1,33 +1,85 @@
-# Echo server program
 import socket
 import sys
+from tqdm import tqdm
+from time import perf_counter
+import pandas as pd
+import argparse
 
-HOST = '127.0.0.1'               # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except OSError as msg:
-        s = None
-        continue
-    try:
-        s.bind(sa)
-        s.listen(1)
-    except OSError as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print('could not open socket')
-    sys.exit(1)
-conn, addr = s.accept()
-with conn:
-    print('Connected by', addr)
-    while True:
-        data = conn.recv(1024)
-        if not data: break
-        conn.send(data)
+def saveImage(filename, data):
+    with open('received/' + filename + '.jpg', 'wb') as i:
+        i.write(data)
+
+def runServer(host, port, buffer_size, save=False):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print('Listening ({},{})'.format(host, port))
+
+        conn, addr = s.accept()
+        time_start = perf_counter()
+        with conn:
+            print('Connected by', addr)
+
+            tot_size = 0
+            n_images = 0
+
+            while True:
+                size = conn.recv(4)
+                if not size:
+                    break
+                size = int.from_bytes(size, byteorder='big')
+                tot_size += size
+                data = b''
+
+                with tqdm(total=size, unit='B', unit_scale=True, leave=True) as pbar:
+                    while size > 0:
+                        pbar.set_description(desc='Image #' + str(n_images))
+                        part = conn.recv(buffer_size)
+                        data += part
+                        size -= len(part)
+
+                        pbar.update(len(part))
+
+                        if len(part) < buffer_size: # either 0 or end of data
+
+                            #print('Received image #{}'.format(n_images))
+                            if save:
+                                saveImage(str(n_images), data)
+                            n_images +=1
+
+                            reply = 'server received image #{}'.format(n_images)
+                            conn.sendall(bytes(reply.encode('utf8')))
+                            break
+
+    time_end = perf_counter()
+    time_elapsed = time_end - time_start
+
+    tot_size = tot_size / 1000000
+
+    return {
+        'n_transmissions': n_images, 
+        'total_time (s)': time_elapsed,
+        'avg_time (s)' : time_elapsed/n_images,
+        'total size (Mb)': tot_size,
+        'avg. size (Mb)': tot_size / n_images,
+        'data rate (Mbit/s)' : (tot_size*8)/time_elapsed
+        }
+
+
+if __name__ == '__main__':
+    # Training settings
+    parser = argparse.ArgumentParser(description='TCP server')
+    parser.add_argument('--host', default='127.0.0.1', help='host ip')
+    parser.add_argument('--port', type=int, default=65432, help='port')
+    parser.add_argument('--buffer_size', type=int, default=4096,  help='buffer size')
+    parser.add_argument('--save', action='store_true', help='saving images on server')
+    args = parser.parse_args()
+
+    stats = runServer(args.host, args.port, args.buffer_size, args.save)
+
+    df = pd.DataFrame(columns=['n_transmissions', 'total_time (s)', 'avg_time (s)', 'total size (Mb)', 'avg. size (Mb)', 'data rate (Mbit/s)'])
+    df = df.append(stats, ignore_index=True)
+    print(df)
+
+    
+    
