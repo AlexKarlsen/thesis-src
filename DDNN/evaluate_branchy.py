466bc59fb1fd288f1841ce145bbf347c7c295511
Alex Karlsen <alexkarlsen7@gmail.com>
2019-10-04 14:49:51 +0200

    new model train

--- DDNN/evaluate_branchy.py
+++ DDNN/evaluate_branchy.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
                         help='input batch size for training (default: 1000)')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--model_path', default='models/dense/miniimagenet_10_20191001-121910_model.pth',
+    parser.add_argument('--model_path', default='models/densenet/miniimagenet_100_20191001-175500_model.pth',
                         help='output directory')
     args = parser.parse_args()
 
